ae04cfa2df8af5ac0a8d8ca1104b388f45c9d991
Patil-Kiran <82840059+Patil-Kiran@users.noreply.github.com>
2021-11-16 23:53:50 +0530

    Add files via upload

---
+++ maincalc.js
@@ -0,0 +1,13 @@
+
+var getCalculations = require("./calc");
+console.log(getCalculations);
+console.log(getCalculations.add(10,20));
+console.log(getCalculations.subtract(25,15));
+console.log(getCalculations.multiply(10,20));
+console.log(getCalculations.divide(20,4));
+console.log(getCalculations.square(25));
+console.log(getCalculations.max(101,2001,2203));
+console.log(getCalculations.min(3,1032,34));
+console.log(getCalculations.sum(1,2,3,4,3,4,5,5,6,6,7,7));
+
+
